6dd63a2658f9aee933880e668edd611e63594c09
minuli2002 <minulidissanayake228@gmail.com>
2026-05-30 11:01:58 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold">
+        Tailwind Working
+      </h1>
+    </div>
+  );
+}
+
+export default App;
